3b974157a1ff5617f98ded414d6fcafc745ecc69
Henrique Tesla <124783530+HenriqueTesla@users.noreply.github.com>
2026-09-02 22:52:43 +0000

    Feat: Setup inicial do Projeto Fincalc com juros simples.

---
+++ Fincalc.py
@@ -0,0 +1,8 @@
+def calcular_juros_simples(capital: float, taxa_anual: float, anos: int) -> float:
+    juros = capital * (taxa_anual / 100) * anos
+    return capital + juros
+
+if __name__ == "___main__":
+    print("Iniciando o sistema FinCalc ... ")
+    montante = calcular_juros_simples(1000.0, 5.0, 2)
+    print(f"Juros Simples (R$ 1.000 a 5% por 2 anos): R${montante:.2f}")
